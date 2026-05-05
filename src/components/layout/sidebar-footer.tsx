741b1dd03090984956b41f0d02f95aff19c08bff
horijin <ouzaher.saad@yahoo.fr>
2026-05-05 20:55:16 +0100

    chore: added updater plugin & basic update dialog

--- src/components/layout/sidebar-footer.tsx
+++ src/components/layout/sidebar-footer.tsx
@@ -2,15 +2,21 @@ import { Settings2, History } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useUIStateStore } from "@/lib/store/use-ui-state-store";
+import { UpdaterDialog } from "../updater-dialog";
 
 export const SidebarFooter = () => {
   const settingsOpen = useUIStateStore((state) => state.settingsDialogOpen);
-  const setSettingsOpen = useUIStateStore((state) => state.setSettingsDialogOpen);
+  const setSettingsOpen = useUIStateStore(
+    (state) => state.setSettingsDialogOpen,
+  );
   const changelogOpen = useUIStateStore((state) => state.changelogDialogOpen);
-  const setChangelogOpen = useUIStateStore((state) => state.setChangelogDialogOpen);
+  const setChangelogOpen = useUIStateStore(
+    (state) => state.setChangelogDialogOpen,
+  );
 
   return (
     <div className="flex flex-col gap-2 px-2 mt-auto">
+      <UpdaterDialog />
       <Button
         size="icon"
         variant="ghost"
